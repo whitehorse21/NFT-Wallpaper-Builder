608530766f2b65b1cb5c9c8c2b5f34c7a31f581b
goldendragondev219 <130621067+goldendragondev219@users.noreply.github.com>
2023-06-07 20:06:19 +0200

    mission done

---
+++ src/pages/missions/cardMission.tsx
@@ -0,0 +1,67 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { CardActionArea } from '@mui/material';
+import Box from '@mui/material/Box';
+
+import logo from '../../assets/icon/logo.svg'
+import photo from '../../assets/images/cards/contemplative-reptile.jpeg'
+
+import { AppProps } from '../../types/AppProps'
+
+ 
+export default function CardMission( props:any) {
+    return (
+        <Card 
+            sx={{ maxWidth: 486 }} 
+            variant='elevation'
+            elevation={0}
+            >
+            <CardActionArea 
+                sx={{ 
+                    display: 'flex', 
+                    alignItems: 'flex-start',
+                    flexDirection: () =>
+                         props.direction === 'row-reverse'
+                            ? 'row-reverse'
+                            : 'row'
+                    }}
+                >
+                <CardMedia
+                    component="img"
+                    height="131"
+                    image= {props.url}
+                    alt="placeholder"
+                    sx={{ p: 2, width: 191 }}
+                />
+                <CardContent >
+                    <Box sx={{
+                        typography: 'h6',
+                        fontSize: 16,
+                        textDecoration: 'underline',
+                        mt: '-5px'
+                    }}>
+                        {props.sectionTitle}
+                    </Box>
+                    <Box sx={{
+                        typography: 'body2',
+                        fontSize: 14,
+                        mt: '5px'
+                    }}>
+                        {props.featureName}
+                    </Box>
+                    <Box sx={{
+                        typography: 'body2',
+                        fontSize: 12,
+                        color: 'text.secondary',
+                        whiteSpace: 'pre-line',
+                    }}>
+                        {props.featureDescription}
+                  </Box>
+                </CardContent>
+            </CardActionArea>
+        </Card>
+    );
+}
